5753d824733b9fd4f465e3e817b7f97bb50248c8
nkleinmann <nekleinmann@gmail.com>
2020-11-05 22:17:46 -0500

    set up basic components in react

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React from "react";
+import Thumbnail from './Thumbnail';
+// import './App.css';
 
 function Projects(props) {
     return (
         <div>
             <h1>Projects</h1>
+            <Thumbnail
+            link="/"
+            image="https://i.imgur.com/qE2okvc.png"
+            title="Nicole Kleinmann"
+            category="Profile"
+            />
         </div>
     )
 }

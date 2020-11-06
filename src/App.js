2a0987aa9c420c22c2904801297d70594099a507
nkleinmann <nekleinmann@gmail.com>
2020-11-05 22:34:10 -0500

    3rd link says contact info

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 // Imports the BrowserRouter, Route and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Projects from './components/Projects.js'
-import Articles from './components/Articles.js'
+import Contact from './components/ContactInfo.js'
 import About from './components/About.js'
 import './App.css';
 
@@ -11,20 +11,20 @@ function App() {
   return (
     <BrowserRouter>
 
-    <div className="App">
-    {/* Set up Router */}
-    <Route exact path="/" component={Projects} />
-    <Route path="/articles" component={Articles} />
-    <Route path="/about" component={About} />
+      <div className="App">
+        {/* Set up Router */}
+        <Route exact path="/" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
 
-      <div className="navigation">
-        <div className="navigation-sub">
-          <Link to="/" className="item">Project</Link>
-          <Link to="/articles" className="item">Articles</Link>
-          <Link to="/about" className="item">About</Link>
+        <div className="navigation">
+          <div className="navigation-sub">
+            <Link to="/" className="item">About</Link>
+            <Link to="/projects" className="item">Project</Link>
+            <Link to="/contact" className="item">Contact Info</Link>
+          </div>
         </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 }

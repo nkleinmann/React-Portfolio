eb1533a616df6733105efc2e3d91e9782cb56e87
nkleinmann <nekleinmann@gmail.com>
2020-11-05 22:48:55 -0500

    deleted navigation and fixed error due to component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 // Imports the BrowserRouter, Route and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Navigation from './components/navigation/navigation.js';
 import Projects from './components/Projects.js'
 import Contact from './components/ContactInfo.js'
 import About from './components/About.js'
@@ -13,17 +14,20 @@ function App() {
 
       <div className="App">
         {/* Set up Router */}
+
+        <div className="row navigation">
+          <div className=" col-12 navigation-sub">
+            <Link to="/" className="item">About Me </Link>
+            <Link to="/projects" className="item">Projects </Link>
+            <Link to="/contact" className="item">Contact Info</Link>
+          </div>
+        </div>
+
         <Route exact path="/" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Projects} />
 
-        <div className="navigation">
-          <div className="navigation-sub">
-            <Link to="/" className="item">About</Link>
-            <Link to="/projects" className="item">Project</Link>
-            <Link to="/contact" className="item">Contact Info</Link>
-          </div>
-        </div>
+      
       </div>
     </BrowserRouter>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react"
 function About(props) {
     return (
         <div>
-            <h1>About</h1>
+            <h1>About Me</h1>
             <img src="https://i.imgur.com/qE2okvc.png" className="profileImage img-fluid" width="260" height="260" alt="Logo Image" />
         </div>
     )

65179d74b937a964815de15ee922803d30985d77
nkleinmann <nekleinmann@gmail.com>
2020-11-07 09:01:51 -0500

    installed bootstrap and bootstrap is now reading correctly

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 // Imports the BrowserRouter, Route and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Navigation from './components/navigation/navigation.js';
 import Projects from './components/Projects.js'
 import Contact from './components/ContactInfo.js'
 import About from './components/About.js'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -15,9 +15,10 @@ function App() {
       <div className="App">
         {/* Set up Router */}
 
-        <div className="row navigation">
+{/* Resource: https://www.framer.com/blog/posts/react-portfolio/ */}
+        <div className="row navigation navbar navbar-light bg-light">
           <div className=" col-12 navigation-sub">
-            <Link to="/" className="item">About Me </Link>
+            <Link to="/" className="item navbar-brand">About Me </Link>
             <Link to="/projects" className="item">Projects </Link>
             <Link to="/contact" className="item">Contact Info</Link>
           </div>
--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,25 @@ import React from "react"
 function About(props) {
     return (
         <div>
-            <h1>About Me</h1>
-            <img src="https://i.imgur.com/qE2okvc.png" className="profileImage img-fluid" width="260" height="260" alt="Logo Image" />
+            <div className="row">
+                <div className="col-12">
+                    <h1>About Me</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-8">
+                    <img src="https://i.imgur.com/qE2okvc.png" className="profileImage img-fluid" width="260" height="260" alt="Logo Image" />
+                </div>
+                <div className="col-4">
+                    <p>My name is Nicole Kleinmann and I am currently a Math Curriculum and Content Developer at <a href="https://mathspace.co/us" target="_blank">Mathspace</a>.
+                        Prior to working at Mathspace, I was a High School Math and Computer Science teacher for
+                        four years. Currently enrolled in a coding bootcamp in full stack web development.
+                        <br /><br />
+                        I am passionate about math and computer science education, web development, and being active. Outside of work I love running or doing
+                        anything outside,playing the piano, and traveling.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }

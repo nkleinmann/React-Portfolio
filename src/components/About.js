4baca5060d300d133a5e7a9d84b13500415453c9
nkleinmann <nekleinmann@gmail.com>
2020-11-07 23:28:22 -0500

    fixed max width of backgound image

--- src/components/About.js
+++ src/components/About.js
@@ -2,23 +2,23 @@ import React from "react";
 
 function About(props) {
     return (
-        <div className="about home p-3">
+        <div className="home">
             <div className="row">
                 <div className="col-12">
                     <h1>About Me</h1>
                 </div>
             </div>
             <br />
-            <div className="row m-3 about">
+            <div className="row about">
                 <div className="col-md-6 col-sm-12">
                     <img src="https://i.imgur.com/qE2okvc.png" className="profileImage img-fluid" width="300" height="300" alt="Logo Image" />
                 </div>
                 <br />
-                <div className="col-md-6 col-sm-12 p-3">
+                <div className="col-md-6 col-sm-12">
                     <p>My name is Nicole Kleinmann and I just completed a coding bootcamp in full stack web development. I am currently a Math Curriculum and Content Developer at <a href="https://mathspace.co/us" target="_blank">Mathspace</a>.
                         Prior to working at Mathspace, I was a High School Math and Computer Science teacher for
-                        four years.
-                        <br /><br />
+                        four years.</p>
+                        <p>
                         I am passionate about math and computer science education, web development, and being active. Outside of work I love running or doing
                         anything outside (especially by the ocean), playing the piano, and traveling.
                     </p>
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -4,7 +4,19 @@ function Contact(props) {
     return (
         <div className="home">
             <h1>Contact Info</h1>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="0.6" d="M0,160L60,186.7C120,213,240,267,360,245.3C480,224,600,128,720,106.7C840,85,960,139,1080,144C1200,149,1320,107,1380,85.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg> */}
+            <div class="row">
+                        <div className="col-sm-12 col-md-4 text-center">
+                            <a href="https://github.com/nkleinmann" target="_blank"><i className="fa fa-github fa-3x"></i> </a>
+                        </div>
+                        <div className="col-sm-12 col-md-4 text-center">
+                            <a href="https://www.linkedin.com/in/nicolekleinmann/" target="_blank"><i
+                                    className="fab fa-linkedin fa-3x"></i></a>
+                        </div>
+                        <div className="col-sm-12 col-md-4 text-center">
+                            <a href="./Images/NK2020resume.pdf" target="_blank"><i
+                                    className="far fa-file fa-3x"></i></a>
+                        </div>
+                    </div>
         </div>
     )
 }

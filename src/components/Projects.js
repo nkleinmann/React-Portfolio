2100cea4b087c26fff8b7047206c759ee5d39309
nkleinmann <nekleinmann@gmail.com>
2020-11-08 17:18:41 -0500

    techUsed updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,37 @@
 import React from "react";
 import projects from "../utils/projectData";
 import Thumbnail from './Thumbnail';
-// import './App.css';
 
 function Projects(props) {
     return (
         <div>
             {/* key={props.portfolioInfo.name.toString()} */}
             <div className="row transparent">
-                <div className="col-4 text-center">
+                <div className="col-md-4 col-sm-12 text-center">
                     <h4>{props.portfolioInfo.name}</h4>
                     <img src={props.portfolioInfo.image} width="260" height="260" alt={props.portfolioInfo.name} />
                 </div>
-                    <div className="col-8">
+                    <div className="col-md-8 col-sm-12 mt-5">
                         <p>{props.portfolioInfo.description}</p>
+                        <h5> Technology used:</h5>
+                        <p>{props.portfolioInfo.techUsed}</p>
+                        {/* <ul id="techList">
+                            {
+                            props.portfolioInfo.techUsed.forEach(element => {
+                                // console.log(element);
+                                let newLiEl = document.createElement("LI");
+                                let text = document.createTextNode(element);
+                                newLiEl.appendChild(text);
+                                // console.log(text);
+                                // console.log("break")
+                                // console.log(newLiEl);
+                                document.getElementById("techList").append(newLiEl);
+                            })
+                            }
+                        </ul> */}
+                        <a href={props.portfolioInfo.githubLink} target="_blank">Github Repository</a>
+                        <br />
+                        <a href={props.portfolioInfo.application} target="_blank">Deployed Site</a>
                     </div>
             </div>
         </div>
--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -5,17 +5,7 @@ const projects = [
         name: "Team Builder",
         description: "Due to the shift to a remote workforce, it can be difficult for teams to create and maintain relationships. This group application aims to help build teams, foster creativity, and strengthen culture.",
         image: teamBuilderImage,
-        techUsed: [
-            "HTML/Bootstrap CSS",
-            "Font Awesome/Mimic.css",
-            "Express Handlebars",
-            "jQuery/Auth0",
-            "ExpressJS/Sessions",
-            "File Stack",
-            "Heroku",
-            "MySQL/Sequelize",
-            "Passport"
-        ],
+        techUsed: "HTML/Bootstrap CSS, Font Awesome/Mimic.css, Express Handlebars, jQuery/Auth0, ExpressJS/Sessions, File Stack, Heroku, MySQL/Sequelize, Passport",
         githubLink: "https://github.com/nkleinmann/the-team-builders",
         application: "https://the-team-builder.herokuapp.com/"
     }

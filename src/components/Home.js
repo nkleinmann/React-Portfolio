31d4aa99a3ce04ec2acad1c572c6ed6b243108b5
nkleinmann <nekleinmann@gmail.com>
2020-11-08 08:21:32 -0500

    centered quote on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import React from "react";
 function Home(props) {
     return (
         <div className="row home">
-            <div className="col-12 justify-content-center">
-                <h3 className="center">"Live as if you were to die tomorrow. Learn as if you were to live today."</h3>
+            <div className="col-12 my-auto">
+                <h3>"Live as if you were to die tomorrow. Learn as if you were to live today."</h3>
                 <h4>~ Mahatma Gandhi</h4>
 
                 {/* <h3 className="center">"You're braver than you believe, stronger than you seem, and smarter than you think"</h3>

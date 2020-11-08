e3d558291486c6a4c0ee30f4bd390e72210f22bc
nkleinmann <nekleinmann@gmail.com>
2020-11-08 00:11:10 -0500

    Added footer

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -5,10 +5,10 @@ function Contact(props) {
         <div className="home">
             <h3>Contact Info</h3>
             <div class="row transparent">
-                <div className="col-12 text-center">
+                <div className="col-6 text-center">
                     <a href="https://github.com/nkleinmann" target="_blank"><i className="fa fa-github fa-3x"></i> </a>
                 </div>
-                <div className="col-12 text-center">
+                <div className="col-6 text-center">
                     <a href="https://www.linkedin.com/in/nicolekleinmann/" target="_blank"><i
                         className="fab fa-linkedin fa-3x"></i></a>
                 </div>
